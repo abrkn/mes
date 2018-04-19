e93a7a6286e6dc89e432c4868d8e2e5307eb35b7
Andreas Brekken <andreas@brekken.com>
2018-04-19 20:03:20 +0700

    Auto expand images

--- index.js
+++ index.js
@@ -34,4 +34,23 @@ $(() => {
         .match(/(\d+)\)/)[1],
     });
   }
+
+  // Auto expand images
+  $('.post .message').each((_, message) => {
+    const $message = $(message);
+    const messageWidth = $message.width();
+    const $a = $message.find('a');
+
+    const $aToImg = $a.filter((_, a) =>
+      $(a)
+        .attr('href')
+        .match(/\.(png|jpe?g|gif)$/i)
+    );
+    $aToImg.each((_, a) =>
+      $(a).html(
+        `<img src="${$a.attr('href')}" style="max-width:${messageWidth -
+          20}px;max-height:200px;" />`
+      )
+    );
+  });
 });
