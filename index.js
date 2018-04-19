2ae49c63f83d7d4a8d2c4da05b3c55ec20902dc2
Andreas Brekken <andreas@brekken.com>
2018-04-19 22:14:54 +0700

    Add in-line liking

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // ==UserScript==
-// @name         Auto-fill Memo-cash password
+// @name         MES
 // @namespace    https://tampermonkey.net/
 // @version      0.3.3
-// @description  Auto-fill Memo.cash Password with Tampermonkey for Chrome
+// @description  Memo Enhancement Suite
 // @author       Andreas Brekken: https://memo.cash/profile/149o1esm1LrYEy1DizZgxANSppx3FESHKw
 // @match        https://memo.cash/*
-// @grant        none
+// @grant        GM_xmlhttpRequest
 // ==/UserScript==
 
 $(() => {
@@ -52,5 +52,76 @@ $(() => {
           20}px;max-height:200px;" />`
       )
     );
+  });
+
+  // In-line-comment
+  $(`.post .actions a[href^='memo/like']`).click(e => {
+    const $a = $(e.target);
+    const href = $a.attr('href');
+    const txHash = href.match(/[^\/\?]+$/)[0];
+    const { memoPassword: password } = localStorage;
+
+    $a.text('Liking Memo...');
+    $a.css('background-color', '#544c40');
+
+    const fetchLikePageAndGetCsrf = () =>
+      new Promise((resolve, reject) => {
+        const url = 'https://memo.cash/' + href;
+
+        console.log(`Fetching like page to get CSRF at ${url}...`);
+
+        const res = GM_xmlhttpRequest({
+          method: 'GET',
+          url,
+          onerror: error => reject(error),
+          onload: res => {
+            console.log(res);
+            console.log(res.responseText);
+            const csrf = res.responseText.match(/MemoApp.InitCsrf."([^"]+)/)[1];
+            resolve(csrf);
+          },
+        });
+      });
+
+    const likePost = csrf =>
+      new Promise((resolve, reject) => {
+        console.log({ csrf });
+
+        const req = {
+          method: 'POST',
+          url: 'https://memo.cash/memo/like-submit',
+          data: `txHash=${txHash}&tip=&password=${password}`,
+          onerror: error => reject(error),
+          overrideMimeType: 'application/x-www-form-urlencoded; charset=UTF-8',
+          headers: {
+            'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+            'x-csrf-token': `${csrf}`,
+            referer: `${href}`,
+          },
+          onload: res => {
+            console.log(res);
+            resolve(res.responseText);
+          },
+        };
+
+        console.log({ req });
+
+        const res = GM_xmlhttpRequest(req);
+      });
+
+    Promise.resolve(1)
+      .then(fetchLikePageAndGetCsrf)
+      .then(likePost)
+      .then(() => {
+        $a.text('Liked Memo!');
+        $a.attr('disabled', 'true');
+        $a.css('background-color', 'rgb(0, 0, 0, 0.2)');
+      })
+      .catch(error => {
+        console.error('oops');
+        console.error(error, error.stack || error.message);
+      });
+
+    return false;
   });
 });
