7c6519b5c959240263819750440ff6a2bd18bb31
Andreas Brekken <andreas@brekken.com>
2018-04-21 08:44:41 +0700

    Extract common Tampermonkey fetches

--- src/app.js
+++ src/app.js
@@ -8,55 +8,11 @@
 // @grant        GM_xmlhttpRequest
 // ==/UserScript==
 
+const { fetchTmXhr, fetchCsrf } = require("./memoFetch");
+
 const MES_STORAGE_KEY = "mes-7932a97f";
 
 const state = JSON.parse(localStorage[MES_STORAGE_KEY] || "{}");
-
-const fetchTmXhr = req =>
-  new Promise((resolve, reject) => {
-    if (!req.url.match(/^http/)) {
-      req.url = `https://memo.cash/${req.url}`;
-    }
-    console.log(`Fetching ${req.url} to get CSRF...`);
-
-    const onerror = error => reject(error);
-
-    const onload = res => {
-      const { readyState, responseText, status } = res;
-
-      if (res.readyState !== 4) {
-        reject(new Error(`readyState=${readyState}; responseText: ${responseText || "<none>"}`));
-        return;
-      }
-      if (!(status >= 200 && status < 300)) {
-        reject(new Error(`HTTP Status=${status || "<none>"}; responseText: ${responseText || "<none>"}`));
-        return;
-      }
-      console.log(`Fetched ${req.url}`);
-
-      resolve(responseText);
-    };
-
-    const contentType = "application/x-www-form-urlencoded; charset=UTF-8";
-
-    const reqToSend = Object.assign(
-      {
-        method: req.data ? "POST" : "GET",
-        headers: {
-          ...(req.csrf ? { "x-csrf-token": `${req.csrf}` } : {}),
-          ...(req.data ? { referer: req.url } : {}),
-          "content-type": contentType
-        },
-        onerror,
-        onload
-      },
-      req
-    );
-
-    GM_xmlhttpRequest(reqToSend);
-  });
-
-const fetchCsrf = url => fetchTmXhr({ url }).then(text => text.match(/MemoApp.InitCsrf."([^"]+)/)[1]);
 
 const likePostInBackground = async (txhash, tip = 0) => {
   const { memoPassword: password } = localStorage;
@@ -81,7 +37,7 @@ const likePostInBackground = async (txhash, tip = 0) => {
 
 const addCss = () => {
   const element = document.createElement("style");
-  element.innerText = require("./app.css");
+  element.innerText = require("../app.css");
 
   document.head.appendChild(element);
 };
---
+++ src/memoFetch.js
@@ -0,0 +1,50 @@
+const fetchTmXhr = req =>
+  new Promise((resolve, reject) => {
+    if (!req.url.match(/^http/)) {
+      req.url = `https://memo.cash/${req.url}`;
+    }
+    console.log(`Fetching ${req.url} to get CSRF...`);
+
+    const onerror = error => reject(error);
+
+    const onload = res => {
+      const { readyState, responseText, status } = res;
+
+      if (res.readyState !== 4) {
+        reject(new Error(`readyState=${readyState}; responseText: ${responseText || "<none>"}`));
+        return;
+      }
+      if (!(status >= 200 && status < 300)) {
+        reject(new Error(`HTTP Status=${status || "<none>"}; responseText: ${responseText || "<none>"}`));
+        return;
+      }
+      console.log(`Fetched ${req.url}`);
+
+      resolve(responseText);
+    };
+
+    const contentType = "application/x-www-form-urlencoded; charset=UTF-8";
+
+    const reqToSend = Object.assign(
+      {
+        method: req.data ? "POST" : "GET",
+        headers: {
+          ...(req.csrf ? { "x-csrf-token": `${req.csrf}` } : {}),
+          ...(req.data ? { referer: req.url } : {}),
+          "content-type": contentType
+        },
+        onerror,
+        onload
+      },
+      req
+    );
+
+    GM_xmlhttpRequest(reqToSend);
+  });
+
+const fetchCsrf = url => fetchTmXhr({ url }).then(text => text.match(/MemoApp.InitCsrf."([^"]+)/)[1]);
+
+Object.assign(exports, {
+  fetchTmXhr,
+  fetchCsrf
+});
