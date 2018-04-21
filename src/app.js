a19354344e88ecb40b93ec5508e82c0f679944a3
Andreas Brekken <andreas@brekken.com>
2018-04-21 09:47:52 +0700

    Add auto linting. We've jumped the shark. Sorry.

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
-const injectInlineCommenting = require("./plugins/inline-commenting");
-const injectRememberPassword = require("./plugins/remember-password");
-const injectAutoExpandImages = require("./plugins/auto-expand-images");
-const injectFixTipAmountInput = require("./plugins/fix-tip-amount");
-const injectCssStateClasses = require("./plugins/css-state-classes");
+const injectInlineCommenting = require('./plugins/inline-commenting');
+const injectRememberPassword = require('./plugins/remember-password');
+const injectAutoExpandImages = require('./plugins/auto-expand-images');
+const injectFixTipAmountInput = require('./plugins/fix-tip-amount');
+const injectCssStateClasses = require('./plugins/css-state-classes');
 
 const addCss = () => {
-  const element = document.createElement("style");
-  element.innerText = require("../app.css");
+  const element = document.createElement('style');
+  element.innerText = require('../app.css');
 
   document.head.appendChild(element);
 };
