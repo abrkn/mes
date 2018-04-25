57fd26783544b839d500b8478c3242af1be20e57
Andreas Brekken <andreas@brekken.com>
2018-04-25 12:58:32 +0700

    Add suffix to UserScript name for NODE_ENV other than production/blank

--- scripts/userscript-header.js
+++ scripts/userscript-header.js
@@ -1,8 +1,11 @@
 #!/usr/bin/env node
-const { version, name, author, homepage, description } = require("../package.json");
+const { version, name, author, homepage, description } = require('../package.json');
+const { NODE_ENV } = process.env;
+
+const nameSuffix = NODE_ENV && NODE_ENV !== 'production' ? `-${NODE_ENV}` : '';
 
 console.log(`// ==UserScript==
-// @name         ${name}
+// @name         ${name}${nameSuffix}
 // @namespace    ${homepage}
 // @version      ${version}
 // @description  ${description}
